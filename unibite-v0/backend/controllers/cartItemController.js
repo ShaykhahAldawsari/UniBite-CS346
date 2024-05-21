4702898ed36e2fe246b0a045293fa68a3be72bc4
ShaykhahAldawsari <124034904+ShaykhahAldawsari@users.noreply.github.com>
2024-05-21 23:50:28 +0300

    Add files via upload

---
+++ unibite-v0/backend/controllers/cartItemController.js
@@ -0,0 +1,7 @@
+const CartItem = require("../models/CartItem");
+
+const addCartItem = async (req, res) => {};
+
+const searchCartItem = async (req, res) => {};
+
+module.exports = { addCartItem, searchCartItem };
